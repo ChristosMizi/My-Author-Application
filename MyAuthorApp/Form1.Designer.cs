89776755cd023fe044a9177e279b03df217d1468
Christos Mizikakis <cmizikakis@gmail.com>
2019-04-03 23:53:10 +0300

    Create the Form drop the DataGridView and the button and name the properties

--- MyAuthorApp/Form1.Designer.cs
+++ MyAuthorApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MyAuthorApp
 {
-    partial class Form1
+    partial class FrmDatabase
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grdBooks = new System.Windows.Forms.DataGridView();
+            this.btnAllRecords = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grdBooks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdBooks
+            // 
+            this.grdBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdBooks.Location = new System.Drawing.Point(26, 12);
+            this.grdBooks.Name = "grdBooks";
+            this.grdBooks.Size = new System.Drawing.Size(748, 284);
+            this.grdBooks.TabIndex = 0;
+            // 
+            // btnAllRecords
+            // 
+            this.btnAllRecords.Location = new System.Drawing.Point(666, 399);
+            this.btnAllRecords.Name = "btnAllRecords";
+            this.btnAllRecords.Size = new System.Drawing.Size(105, 23);
+            this.btnAllRecords.TabIndex = 1;
+            this.btnAllRecords.Text = "Show All Records";
+            this.btnAllRecords.UseVisualStyleBackColor = true;
+            // 
+            // FrmDatabase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnAllRecords);
+            this.Controls.Add(this.grdBooks);
+            this.Name = "FrmDatabase";
+            this.Text = "Book Database";
+            ((System.ComponentModel.ISupportInitialize)(this.grdBooks)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView grdBooks;
+        private System.Windows.Forms.Button btnAllRecords;
     }
 }
 
